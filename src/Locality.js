235d9043e617e932a9254829a76a93eee0695b94
Vensy <pierrevensy@gmail.com>
2020-07-23 10:47:18 -0400

    Added new records to submunicipalities.json and new methods to Locality.js

--- src/Locality.js
+++ src/Locality.js
@@ -2,6 +2,7 @@ import county from './County'
 import district from './District'
 import municipality from './Municipality'
 import submunicipality from './SubMunicipality'
+import normalize from './normalize'
 
 class Locality {
 
@@ -55,6 +56,38 @@ class Locality {
             items.concat(submunicipality.getByMunicipality(v))
         })
         return items
+    }
+
+    find (name) {
+        if (county.has(name)) return county.get(name)
+        if (district.has(name)) {
+            const item = district.get(name)
+            return {
+                county: item.county,
+                ...item.districts.find(v => normalize(v.name) === normalize(name))
+            }
+        }
+        if (municipality.has(name)) {
+            const item = municipality.get(name)
+            const district = district.get(item.district)
+            return {
+                county: district.county,
+                district: item.county,
+                ...item.municipalities.find(v => normalize(v.name) === normalize(name))
+            }
+        }
+        if (submunicipality.has(name)) {
+            const item = submunicipality.get(name)
+            const municipality = municipality.get(item.municipality)
+            const district = district.get(municipality.district)
+            return {
+                county: district.county,
+                district: municipality.county,
+                municipality: item.municipality,
+                ...item.submunicipalities.find(v => normalize(v.name) === normalize(name))
+            }
+        }
+        return
     }
 }
 
